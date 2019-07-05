102c75d48f8d044cfb7643a491ebd15932f7a2a3
William Reyes <williamreyes814@yahoo.com>
2019-07-05 19:23:12 -0400

    Done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,10 +5,34 @@ public class Music {
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        // Find the smallest number of jumps required to get to specified(selection) song in playlist
+
+        int countRight = 0;
+        int countLeft = 0;
+
+        for (int i = startIndex; selection != playList[i]; i++) {
+            countRight++;
+
+            if(i == playList.length - 1 )
+                i = 0;
+        }
+
+        for (int i = startIndex; selection != playList[i]; i--) {
+                countLeft++;
+
+            if( i == 0)
+                i = playList.length;
+
+        }
+
+        if(countLeft < countRight)
+            return countLeft;
+
+        else return countRight;
     }
 }
